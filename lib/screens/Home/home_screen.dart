0cce840daa1ee35391a3bd80e08ab81060fe39e0
Awaizkazi <Owaizkazi789@gmail.com>
2024-05-05 15:41:22 +0530

    Making Home Screen

--- lib/screens/Home/home_screen.dart
+++ lib/screens/Home/home_screen.dart
@@ -1,10 +1,39 @@
+import 'package:e_commerce_app_1/screens/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  IconButton(
+                    style: IconButton.styleFrom(
+                      backgroundColor: kcontentColor,
+                      padding: const EdgeInsets.all(20.0),
+                    ),
+                    onPressed: () {},
+                    icon: Image.asset(
+                      'images/icon.png',
+                      height: 20,
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
